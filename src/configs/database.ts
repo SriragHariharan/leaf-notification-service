378f485603a9225fbc3ca9eec10c9ac5ce6fd552
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-02-20 11:16:43 +0530

    Added RabbitMQ to recieve user events like user signup, update username or update profile picture.

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -4,10 +4,7 @@ async function connectToDatabase() {
     const databaseURL = process.env.DATABASE_URL;
 
     try {
-        await mongoose.connect(databaseURL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(databaseURL);
         console.log('Connected to database...');
     } catch (error) {
         console.error('Error connecting to MongoDB Atlas:', error);
